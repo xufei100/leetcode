6fd820da188181df7b0e0d0580696a83d72d339b
xufei <1842070912@qq.com>
2019-06-30 12:04:08 +0800

    修改函数格式

--- 001.c
+++ 001.c
@@ -3,13 +3,15 @@
 #define ARRAY_SIZE  5
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
-{
+{  
+   int* result = malloc(2 * sizeof(int));
    for (int i = 0; i < numsSize - 1; i++) {
        for(int j = i + 1; j < numsSize; j++) {
               if (nums[i] + nums[j] == target) {
-                  returnSize[0] = i;
-                  returnSize[1] = j;
-                  return returnSize;
+                  result[0] = i;
+                  result[1] = j;
+                  *returnSize = 2;
+                  return result;
               }
         }
     }
@@ -20,13 +22,13 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 int main()
 {
   int* nums = (int []){1, 3, 2, 7, 11, 15, 18, 19, 20};
-  int* result = malloc(2 * sizeof(int));
-  int* rs;
-  rs = twoSum(nums, ARRAY_SIZE, 9, result);
+  int  *returnSize, *rs;
+  int i = 0;
+  returnSize = &i;
+  rs = twoSum(nums, ARRAY_SIZE, 9, returnSize);
   printf("%d %d \n", rs[0], rs[1]);
-  printf("%d %d \n", result[0], result[1]);
-  while (*result != 0) {
+  /*while (*result != 0) {
      printf("%d  ",*result);
      result++;
-  }
+  }*/
 } 
